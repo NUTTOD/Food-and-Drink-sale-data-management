95c82ce8d41f20e9395f0c01c04ca9d4745cada1
NUTTOD <nuttod88@gmail.com>
2025-09-19 20:14:34 +0700

    19-9-2025 Funtion Read CSV file v1.1.0

--- project/Food-and-Drink-sale-data-management.c
+++ project/Food-and-Drink-sale-data-management.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_symbol(int length, char symbol){
+    for (int i = 0; i < length; i++){
+        putchar(symbol);
+    }
+    putchar('\n');
+}
 
 int ReadCSV (){
+    char buffer [1024];
     FILE *CSVFile = fopen("csvfile/Food-and-Drink-sale-data-management.csv", "r");
+
     if (CSVFile == NULL){
         printf("File %p\n", CSVFile);
         printf("เปิดไฟล์ไม่สำเร็จ\n");
@@ -9,15 +20,28 @@ int ReadCSV (){
     else{
         printf("File %p\n", CSVFile);
         printf("เปิดไฟล์สำเร็จ\n");
+
+        if (fgets(buffer, sizeof(buffer), CSVFile) != NULL){
+            char *header_token = strtok(buffer, ",\n");
+            while (header_token != NULL){
+                printf("%-35s", header_token);
+                header_token = strtok(NULL, ",\n");
+            }
+            printf("\n");
+            print_symbol(120, '=');
+        }
+
         fclose(CSVFile);
     }
+
 }
 
 int main(){
     int menu = 0;
+
     while(menu != 7){
         printf("ระบบจัดการข้อมูลการซื้ออาหารและเครื่องดื่ม\n");
-        printf("---------โปรดเลือกฟังก์ชันที่ต้องการ---------\n");
+        printf("------------โปรดเลือกฟังก์ชันที่ต้องการ------------\n");
         printf("1.อ่านข้อมูลการซื้อ\n");
         printf("2.บันทึกข้อมูลการซื้อ\n");
         printf("3.เพิ่มข้อมูลการซื้อ\n");
@@ -27,12 +51,12 @@ int main(){
         printf("7.ออกจากโปรแกรม");
         printf("เลือกฟังก์ชันโดยตัวเลข 1-7 : ");
         scanf("%d", &menu);
-        printf("-------------------------------------\n");
+        print_symbol(44, '-');
 
         switch (menu){
             case 1: 
                 ReadCSV();
-                printf("-------------------------------------\n");
+                print_symbol(44, '-');
                 break;
             case 2:
                 break;
